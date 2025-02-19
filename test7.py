b90daff7e619bba0fdf542de90a3658d71e8f779
rhsy4 <enhbate245@gmail.com>
2025-02-19 13:29:48 +0800

    C1

---
+++ test7.py
@@ -0,0 +1,9 @@
+a = int(float(input("хурдаа оруулан уу: ")))
+if a < 10:
+    print("удаан")
+elif a < 50:
+    print("дундаж")
+elif a < 150:
+    print("хурдан")
+elif a < 1000:
+    print("маш хурдан")
